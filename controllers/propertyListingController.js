0a7cf8b1f5618b6bc5953a3dec114fa9429e7fa5
Ezekiel Afolabi <ezekialafolabi11@gmail.com>
2024-06-26 19:57:41 +0100

    Added Property listing Updating And deleting endpoint

--- controllers/propertyListingController.js
+++ controllers/propertyListingController.js
@@ -128,7 +128,7 @@ const editPropertyListing = async (req, res) => {
   }
 };
 
-const updatePropertyType = async (req, res) => {
+const updatePropertyListing = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res
@@ -137,27 +137,82 @@ const updatePropertyType = async (req, res) => {
   }
 
   try {
-    const { id, propertyType } = req.body;
-    const slug = propertyType.toLowerCase().replace(/\s+/g, "-");
+    // const { id, propertyType } = req.body;
+    const {
+      id,
+      title,
+      listingType,
+      usageType,
+      propertyType,
+      propertySubType,
+      propertyCondition,
+      state,
+      neighbourhood,
+      size,
+      propertyDetails,
+      NoOfLivingRooms,
+      NoOfBedRooms,
+      NoOfKitchens,
+      NoOfParkingSpace,
+      Price,
+      virtualTour,
+      video,
+      photo
+    } = req.body;
+    const slug = title.toLowerCase().replace(/\s+/g, "-");
 
-    const Data = { propertyType, slug };
-    const updated = await PropertyType.findOneAndUpdate({ _id: id }, Data, {
+    const Data = { 
+      title,
+      slug,
+      listingType,
+      usageType,
+      propertyType,
+      propertySubType,
+      propertyCondition,
+      state,
+      neighbourhood,
+      size,
+      propertyDetails,
+      NoOfLivingRooms,
+      NoOfBedRooms,
+      NoOfKitchens,
+      NoOfParkingSpace,
+      Price,
+      virtualTour,
+      video,
+      photo
+    };
+    const objectId = new ObjectId(id);
+    const updated = await PropertyListing.findOneAndUpdate({ _id: objectId }, Data, {
       new: true,
     });
+    var counter = 0;
+    for (const photoEntry of photo) {
+      counter++;
+    }
+    if(counter > 0){
+      await Photos.deleteMany({ propertyListingId: objectId });
+      for (const photoEntry of photo) {
+        await Photos.create({ 
+          propertyListingId: id,
+          path: photoEntry.path 
+        });
+      }
+    }
+    
     if (!updated) {
       return res
         .status(404)
         .json({
           responseCode: 404,
-          responseMessage: "Property Type not found",
+          responseMessage: "Could not update property",
         });
     }
     return res.status(200).json({
       responseCode: 200,
-      responseMessage: "Property Type updated successfully",
-      role: {
-        id: updated.id,
-        propertyType: updated.propertyType,
+      responseMessage: "Property updated successfully",
+      data: {
+        data: updated
       },
     });
   } catch (error) {
@@ -168,19 +223,59 @@ const updatePropertyType = async (req, res) => {
   }
 };
 
-const fetchPropertyType = async (req, res) => {
+const fetchPropertyListing = async (req, res) => {
   try {
     // const users = await User.find();
-    const fetchRcords = await PropertyType.find().select(
-      "slug propertyType createdAt"
-    );
-    res.json(fetchRcords);
+    // const fetchRcords = await PropertyListing.find().select(
+    //   "title slug listingType usageType propertyType propertySubType propertyCondition state neighbourhood size propertyDetails NoOfLivingRooms NoOfBedRooms NoOfKitchens NoOfParkingSpace Price virtualTour video photo createdAt"
+    // );
+    const fetchRecords = await PropertyListing.aggregate([
+      {
+        $addFields: {
+          _idString: { $toString: "$_id" }
+        }
+      },
+      {
+        $lookup: {
+          from: 'photos', // The name of the photos collection
+          localField: '_idString', // The field from PropertyListing as string
+          foreignField: 'propertyListingId', // The field from the photos collection that matches
+          as: 'photos' // The name of the array field to be added to each document
+        }
+      },
+      {
+        $project: {
+          title: 1,
+          slug: 1,
+          listingType: 1,
+          usageType: 1,
+          propertyType: 1,
+          propertySubType: 1,
+          propertyCondition: 1,
+          state: 1,
+          neighbourhood: 1,
+          size: 1,
+          propertyDetails: 1,
+          NoOfLivingRooms: 1,
+          NoOfBedRooms: 1,
+          NoOfKitchens: 1,
+          NoOfParkingSpace: 1,
+          Price: 1,
+          virtualTour: 1,
+          video: 1,
+          photos: 1, // Include the photos array
+          createdAt: 1
+        }
+      }
+    ]);
+    
+    res.json(fetchRecords);
   } catch (error) {
     res.status(500).json({ responseCode: 500, responseMessage: error.message });
   }
 };
 
-const deletePropertyType = async (req, res) => {
+const deletePropertyListing = async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -191,24 +286,25 @@ const deletePropertyType = async (req, res) => {
 
     const { id } = req.body;
     const objectId = new ObjectId(id);
-    const check = await PropertyType.findById({ _id: objectId });
+    const check = await PropertyListing.findById({ _id: objectId });
     if (!check) {
       return res
         .status(404)
         .json({
           responseCode: 404,
-          responseMessage: "Property Type not found",
+          responseMessage: "Property Listing not found",
         });
     }
 
     // Delete the user
     // await User.findByIdAndDelete({ roleId });
-    await PropertyType.findByIdAndDelete({ _id: objectId });
+    await PropertyListing.findByIdAndDelete({ _id: objectId });
+    await Photos.deleteMany({ propertyListingId: objectId });
 
     // Respond with a success message
     return res.status(200).json({
       responseCode: 200,
-      responseMessage: "Property Type deleted successfully",
+      responseMessage: "Property Listing deleted successfully",
     });
   } catch (error) {
     res.status(500).json({ responseCode: 500, responseMessage: error.message });
@@ -218,7 +314,7 @@ const deletePropertyType = async (req, res) => {
 module.exports = {
   createPropertyListing,
   editPropertyListing,
-  updatePropertyType,
-  fetchPropertyType,
-  deletePropertyType,
+  updatePropertyListing,
+  fetchPropertyListing,
+  deletePropertyListing
 };
