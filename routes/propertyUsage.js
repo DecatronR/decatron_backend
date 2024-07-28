3930ce13ea2d95dbd6a717807b30f9726f393e7c
Ezekiel Afolabi <ezekialafolabi11@gmail.com>
2024-07-28 15:48:41 +0100

    Created Property Usage Endpoint

---
+++ routes/propertyUsage.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createPropertyUsage,
+  editPropertyUsage,
+  updatePropertyUsage,
+  fetchPropertyUsage,
+  deletePropertyUsage,
+} = require("../controllers/propertyUsageController");
+const { body } = require("express-validator");
+const { requireAuth } = require("../middleware/authMiddleware");
+
+router.post(
+  "/createPropertyUsage",
+  requireAuth,
+  [
+    body("propertyusage")
+      .notEmpty()
+      .withMessage("Property Usage Name field is required"),
+  ],
+  createPropertyUsage
+);
+
+router.post(
+  "/editPropertyUsage",
+  requireAuth,
+  [body("id").notEmpty().withMessage("Property Usage field is required")],
+  editPropertyUsage
+);
+
+router.post(
+  "/updatePropertyUsage",
+  requireAuth,
+  [
+    body("id").notEmpty().withMessage("Property Usage field is required"),
+    body("propertyusage")
+      .notEmpty()
+      .withMessage("Property Usage field is required"),
+  ],
+  updatePropertyUsage
+);
+
+router.get("/fetchPropertyUsage", requireAuth, fetchPropertyUsage);
+router.post(
+  "/deletePropertyUsage",
+  requireAuth,
+  [body("id").notEmpty().withMessage("Property Usage ID field is required")],
+  deletePropertyUsage
+);
+
+module.exports = router;
