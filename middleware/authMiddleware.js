83c9f2718499c2c6aa3e8f0b5709f8b0527d6e60
koladeolukoya <koladeolukoya@gmail.com>
2024-10-10 12:19:02 +0100

    auth: adding need for headers

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,32 +1,32 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
+const secretKey = process.env.JWT_SECURITY_KEY;
+
 const requireAuth = (req, res, next) => {
-  const token = req.cookies.auth_jwt;
-  // console.log(token);
-  if (token) {
-    jwt.verify(
-      token,
-      "h8BHWDXuW1IPcUHNcCNdsDKucaqHgLzN6ZZT4DMm0LM",
-      (err, decodedToken) => {
-        if (err) {
-          console.log(err.message);
-          // res.redirect('/login');
-          return res.status(403).json({
-            responseMessage:
-              "Kindly Login to access this application facillities",
-            responseCode: 403,
-          });
-        } else {
-          console.log(decodedToken);
-          next();
-        }
+  // Get the Authorization header
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    // Extract the token from the header
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, secretKey, (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        return res.status(403).json({
+          responseMessage:
+            "Kindly Login to access this application's facilities",
+          responseCode: 403,
+        });
+      } else {
+        console.log(decodedToken);
+        next(); // Proceed to the next middleware or route handler
       }
-    );
+    });
   } else {
-    // res.redirect('/login');
     return res.status(403).json({
-      responseMessage: "Kindly Login to access this application facilities",
+      responseMessage: "Kindly Login to access this application's facilities",
       responseCode: 403,
     });
   }
@@ -35,22 +35,18 @@ const requireAuth = (req, res, next) => {
 const checkUser = (req, res, next) => {
   const token = req.cookies.auth_jwt;
   if (token) {
-    jwt.verify(
-      token,
-      "h8BHWDXuW1IPcUHNcCNdsDKucaqHgLzN6ZZT4DMm0LM",
-      async (err, decodedToken) => {
-        if (err) {
-          console.log(err.message);
-          res.locals.user = null;
-          next();
-        } else {
-          console.log(decodedToken);
-          let user = await User.findById(decodedToken.id);
-          res.locals.user = user;
-          next();
-        }
+    jwt.verify(token, secretKey, async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
       }
-    );
+    });
   } else {
     res.locals.user = null;
     next();
