f7116304801e5dbc7c3dd064475f545439c87dc6
koladeolukoya <koladeolukoya@gmail.com>
2024-10-11 07:06:29 +0100

    feat: added data and time to booking route

--- routes/booking.js
+++ routes/booking.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const {
-  create,
-  deleteData
-} = require("../controllers/bookingController");
+const { create, deleteData } = require("../controllers/bookingController");
 const { body } = require("express-validator");
 const { requireAuth } = require("../middleware/authMiddleware");
 
 router.post(
   "/book",
   requireAuth,
-  [body("propertyID").notEmpty().withMessage("Property ID field is required")],
-  [body("userID").notEmpty().withMessage("User ID field is required")],
-  [body("agentID").notEmpty().withMessage("Agent ID field is required")],
+  [
+    body("propertyID").notEmpty().withMessage("Property ID field is required"),
+    body("userID").notEmpty().withMessage("User ID field is required"),
+    body("agentID").notEmpty().withMessage("Agent ID field is required"),
+    body("bookingDateTime")
+      .notEmpty()
+      .withMessage("Booking date and time is required"),
+  ],
   create
 );
 
