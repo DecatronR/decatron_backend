764de8167f18b7e65fdc4592458d4b886dfbec28
Ezekiel Afolabi <ezekialafolabi11@gmail.com>
2024-10-12 14:02:29 +0100

    Completed Property image module

--- routes/users.js
+++ routes/users.js
@@ -42,6 +42,7 @@ const upload = multer({
   }
 });
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
-
-const mongoDbUri = process.env.MONGO_DB_URI;
+// const mongoose = require("mongoose");
 
 mongoose
-  .connect(mongoDbUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb+srv://ezekialafolabi11:GDxEcRBBdGrL6BJ2@cluster0.xglip.mongodb.net/", { })
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
+
+// require("dotenv").config();
+// const mongoDbUri = process.env.MONGO_DB_URI;
+// mongoose
+//   .connect(mongoDbUri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connected to DB"))
+//   .catch((err) => console.log(err));
