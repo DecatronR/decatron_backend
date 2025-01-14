eb14a7f23b2333b32464eceeb574b489f79ffb2b
Ezekiel Afolabi <ezekialafolabi11@gmail.com>
2025-01-14 01:23:14 +0100

    Made adjustment on agent and user agency endpoint

--- controllers/agencyRequestController.js
+++ controllers/agencyRequestController.js
@@ -1,6 +1,6 @@
 const AgencyRequest = require("../models/agencyRequest");
 const { validationResult } = require("express-validator");
-// const { formatRoleId } = require("../utils/helpers");
+const User = require("../models/User");
 const { ObjectId } = require("mongodb");
 
 const create = async (req, res, next) => {
@@ -71,7 +71,34 @@ const deleteRequest = async (req, res) => {
   }
 };
 
-const agentRequest = async (req, res) => {
+// const agentRequest = async (req, res) => {
+//   try {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res
+//         .status(400)
+//         .json({ responseCode: 400, responseMessage: errors.array() });
+//     }
+
+//     const { requestAgentId } = req.body;
+//     // const id = new ObjectId(requestAgentId);
+//     const check = await AgencyRequest.find({ agentId: requestAgentId });
+//     if (!check) {
+//       return res
+//         .status(404)
+//         .json({ responseCode: 404, responseMessage: "No Record found" });
+//     }
+//     const data = await AgencyRequest.find({ agentId: requestAgentId });
+//     return res.status(200).json({
+//       responseMessage: "Record Found",
+//       responseCode: 200,
+//       data: data,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ responseMessage: error.message });
+//   }
+// };
+const agentRequest = async (req, res) => { 
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -82,24 +109,43 @@ const agentRequest = async (req, res) => {
 
     const { requestAgentId } = req.body;
     // const id = new ObjectId(requestAgentId);
-    const check = await AgencyRequest.find({ agentId: requestAgentId });
+    const check = await AgencyRequest.find({ agentId:requestAgentId });
     if (!check) {
       return res
         .status(404)
         .json({ responseCode: 404, responseMessage: "No Record found" });
     }
-    const data = await AgencyRequest.find({ agentId: requestAgentId });
+
+    const results = await Promise.all(
+        (
+          await AgencyRequest.find({ agentId: requestAgentId })
+        ).map(async (request) => {
+
+            const getProperty = await PropertyListing.findOne({ _id: request.propertyListingId});
+            const response = {
+                id: request._id,
+                agentId: request.agentId,
+                agentProp: { propertyName: getProperty.title, location: getProperty.neighbourhood },
+                propertyListingId: request.propertyListingId,
+                status: request.status,
+                ownerId: request.ownerId,
+                createdAt: request.createdAt,
+            };
+
+          return response;
+        })
+      );
+      
     return res.status(200).json({
-      responseMessage: "Record Found",
-      responseCode: 200,
-      data: data,
-    });
+        responseMessage: "Record Found",
+        responseCode: 200,
+        data: results
+      });
   } catch (error) {
     res.status(500).json({ responseMessage: error.message });
   }
-};
-
-const ownerRequest = async (req, res) => {
+}
+const ownerRequest = async (req, res) => { 
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -109,23 +155,76 @@ const ownerRequest = async (req, res) => {
     }
 
     const { ownerId } = req.body;
-    const id = new ObjectId(ownerId);
-    const check = await AgencyRequest.find({ ownerId: id });
+    // const id = new ObjectId(ownerId);
+    const check = await AgencyRequest.find({ ownerId:ownerId });
     if (!check) {
       return res
         .status(404)
         .json({ responseCode: 404, responseMessage: "No Record found" });
     }
-    const data = await AgencyRequest.find({ ownerId: id });
+
+    const results = await Promise.all(
+        (
+          await AgencyRequest.find({ ownerId:ownerId })
+        ).map(async (request) => {
+
+            const getUser = await User.findOne({ _id: request.agentId});
+            const ratings = getUser.ratings;
+            const totalRatings = ratings.length;
+            const sumOfRatings = ratings.reduce((acc, curr) => acc + curr.rating, 0);
+            const averageRating =
+              totalRatings > 0 ? (sumOfRatings / totalRatings).toFixed(1) : 0;
+            const response = {
+                id: request._id,
+                agentId: request.agentId,
+                agentProp: { agentName: getUser.name, rating: parseFloat(averageRating) },
+                propertyListingId: request.propertyListingId,
+                status: request.status,
+                ownerId: request.ownerId,
+                createdAt: request.createdAt,
+            };
+
+          return response;
+        })
+      );
+      
     return res.status(200).json({
-      responseMessage: "Record Found",
-      responseCode: 200,
-      data: data,
-    });
+        responseMessage: "Record Found",
+        responseCode: 200,
+        data: results
+      });
   } catch (error) {
     res.status(500).json({ responseMessage: error.message });
   }
-};
+}
+
+// const ownerRequest = async (req, res) => {
+//   try {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res
+//         .status(400)
+//         .json({ responseCode: 400, responseMessage: errors.array() });
+//     }
+
+//     const { ownerId } = req.body;
+//     const id = new ObjectId(ownerId);
+//     const check = await AgencyRequest.find({ ownerId: id });
+//     if (!check) {
+//       return res
+//         .status(404)
+//         .json({ responseCode: 404, responseMessage: "No Record found" });
+//     }
+//     const data = await AgencyRequest.find({ ownerId: id });
+//     return res.status(200).json({
+//       responseMessage: "Record Found",
+//       responseCode: 200,
+//       data: data,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ responseMessage: error.message });
+//   }
+// };
 module.exports = {
   create,
   deleteRequest,
