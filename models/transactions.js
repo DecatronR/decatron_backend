8becd73e5db8a8ec584e5a592b0182b607237f68
kolexstrings <koladeolukoya@gmail.com>
2025-03-13 12:02:18 +0100

    feat: added timestamp

--- models/transactions.js
+++ models/transactions.js
@@ -1,43 +1,46 @@
 const mongoose = require("mongoose");
 
-const TransactionSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // References the User model
-    required: true,
+const TransactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // References the User model
+      required: true,
+    },
+    paymentReference: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    customerName: {
+      type: String,
+      required: true,
+    },
+    customerEmail: {
+      type: String,
+      required: true,
+    },
+    paymentDescription: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["PENDING", "PAID", "FAILED"],
+      default: "PENDING",
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      default: new Date(),
+    },
   },
-  paymentReference: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  customerName: {
-    type: String,
-    required: true,
-  },
-  customerEmail: {
-    type: String,
-    required: true,
-  },
-  paymentDescription: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: ["PENDING", "PAID", "FAILED"],
-    default: "PENDING",
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: new Date(),
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Transaction", TransactionSchema);
