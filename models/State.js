2d13d6fed84e751f626a7cc8436ab4880ed52761
kolexstrings <koladeolukoya@gmail.com>
2025-03-13 12:01:17 +0100

    update transaction model

--- models/State.js
+++ models/State.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
-const StateSchema = new mongoose.Schema({
-  slug: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true,
-  },
-  state: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+const StateSchema = new mongoose.Schema(
+  {
+    slug: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    state: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-  createdAt: {
-    type: Date,
-    required: true,
-    default: new Date(),
+    createdAt: {
+      type: Date,
+      required: true,
+      default: new Date(),
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("State", StateSchema);
--- models/transactions.js
+++ models/transactions.js
@@ -1,20 +1,43 @@
 const mongoose = require("mongoose");
 
-const transactionSchema = new mongoose.Schema(
-  {
-    paymentReference: { type: String, required: true, unique: true },
-    amount: { type: Number, required: true },
-    customerName: { type: String, required: true },
-    customerEmail: { type: String, required: true },
-    status: {
-      type: String,
-      enum: ["PENDING", "PAID", "FAILED"],
-      default: "PENDING",
-    },
-    paymentDescription: { type: String },
-    currencyCode: { type: String, default: "NGN" },
+const TransactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // References the User model
+    required: true,
   },
-  { timestamps: true }
-);
+  paymentReference: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  customerName: {
+    type: String,
+    required: true,
+  },
+  customerEmail: {
+    type: String,
+    required: true,
+  },
+  paymentDescription: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["PENDING", "PAID", "FAILED"],
+    default: "PENDING",
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
+});
 
-module.exports = mongoose.model("Transaction", transactionSchema);
+module.exports = mongoose.model("Transaction", TransactionSchema);
