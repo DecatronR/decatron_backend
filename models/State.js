e4655cb40929786769cb9f412649a11f44396381
kolexstrings <koladeolukoya@gmail.com>
2025-03-13 12:02:18 +0100

    feat: added timestamp

--- models/State.js
+++ models/State.js
@@ -1,26 +1,23 @@
 const mongoose = require("mongoose");
 
-const StateSchema = new mongoose.Schema(
-  {
-    slug: {
-      type: String,
-      required: true,
-      lowercase: true,
-      unique: true,
-    },
-    state: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    createdAt: {
-      type: Date,
-      required: true,
-      default: new Date(),
-    },
+const StateSchema = new mongoose.Schema({
+  slug: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
   },
-  { timestamps: true }
-);
+  state: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  createdAt: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
+});
 
 module.exports = mongoose.model("State", StateSchema);
