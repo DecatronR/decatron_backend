869906a2a45ca4d0c01823a698e1005f2fe5479d
kolexstrings <koladeolukoya@gmail.com>
2025-04-05 18:55:09 +0100

    feat: service impl

---
+++ services/sendNotification.js
@@ -0,0 +1,18 @@
+const admin = require("./firebaseAdmin");
+
+const sendPushNotification = async (fcmToken, title, body) => {
+  const message = {
+    token: fcmToken,
+    notification: {
+      title,
+      body,
+    },
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    console.log("Successfully sent message:", response);
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
+};
