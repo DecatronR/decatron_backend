d9a83fbab8d606bfb74e50f43ea5f14106bd9ddb
kolexstrings <koladeolukoya@gmail.com>
2025-04-28 22:51:27 +0100

    fix: build firebase config

--- services/firebaseAdmin.js
+++ services/firebaseAdmin.js
@@ -1,20 +1,10 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Handle newline characters in private key
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-};
+const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+const serviceAccount = JSON.parse(
+  Buffer.from(serviceAccountBase64, "base64").toString("utf-8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-module.exports = admin;
