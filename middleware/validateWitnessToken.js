ce86aca5a85262958d84a91a7a5e3f95a730064a
kolexstrings <koladeolukoya@gmail.com>
2025-05-02 03:58:05 +0100

    feat: token validation

---
+++ middleware/validateWitnessToken.js
@@ -0,0 +1,24 @@
+const ESignature = require("../models/ESignature");
+
+const validateWitnessToken = async (req, res, next) => {
+  const { token } = req.query;
+  if (!token)
+    return res.status(401).json({ message: "Invalid or missing token" });
+
+  // Find the signature event associated with the token
+  const signatureEvent = await ESignature.findOne({ signingToken: token });
+  if (!signatureEvent)
+    return res.status(401).json({ message: "Invalid token" });
+
+  // Check if the token has expired
+  const currentDate = new Date();
+  if (currentDate > signatureEvent.tokenExpiresAt) {
+    return res.status(401).json({ message: "Token has expired" });
+  }
+
+  // If token is valid and not expired, attach it to the request
+  req.signatureEvent = signatureEvent;
+  next();
+};
+
+module.exports = validateWitnessToken;
